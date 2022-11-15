733b71b48e1f9d26947f40a614d58a272667668b
FelixLuciano <felix.lucianodias@gmail.com>
2022-11-15 13:25:57 -0300

    Feat(markdown): LaTex

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,19 +1,36 @@
+import katex from 'markdown-it-katex'
+
+
 export default {
   base: '/aguas-da-cidade-jardim/',
+
   lang: 'pt-BR',
   title: 'Monitorando as Águas',
   description: 'Monitorando as Águas da Ponte Cidade Jardim.',
+
   head: [
-    ['link', { rel: 'icon', href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%22-0.1em%22 y=%22.90em%22 font-size=%2286%22>🔬</text></svg>" }]
+    ['link', { rel: 'icon', href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%22-0.1em%22 y=%22.90em%22 font-size=%2286%22>🔬</text></svg>" }],
+    ['link', { rel: 'stylesheet', href: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css" }]
   ],
+
+  markdown: {
+    config: (md) => {
+      // use more markdown-it plugins!
+      md.use(katex)
+    }
+  },
+
   themeConfig: {
     siteTitle: '🔬 Monitorando as Águas',
+
     nav: [
       { text: 'Insper', link: 'https://insper.edu.br' },
     ],
+
     socialLinks: [
       { icon: 'github', link: 'https://github.com/FelixLuciano/aguas-da-cidade-jardim' }
     ],
+
     sidebar: [
       {
         text: 'Introdução',
@@ -29,8 +46,10 @@ export default {
         ]
       }
     ],
+
     outline: 'deep',
     outlineTitle: 'Tópicos',
+
     footer: {
       message: 'Publicado sob a Licença MIT.',
       copyright: 'Copyright © 2022'
